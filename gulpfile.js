798d6fb04bc15e7184ecd3e6605e5e364e86fd38
bbbozhena <bbbozhenka@gmail.com>
2022-07-01 14:04:11 +0300

    add geo

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+gulp.task("sass", function () {
+  return (
+    gulp
+      .src("style.scss")
+      .pipe(sass())
+      .on("error", sass.logError)
+      .pipe(gulp.dest("styles"))
+  );
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./style.scss', gulp.series(['sass']));
+});
---
+++ script.js
@@ -0,0 +1,39 @@
+const city = document.getElementById ('city');
+const dateEl = document.getElementById ('date');
+const tempEl = document.getElementById ('temp');
+const weatherEl = document.getElementById ('weather');
+const weatherItem = document.querySelector ('weather-item');
+
+const API_KEY = '734dfb31bce7daf473e8a526d99d3812';
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+
+setInterval (() => {
+    const time = new Date ();
+    const month = time.getMonth();
+    const date =  time.getDate();
+    const day = time.getDay();
+
+    dateEl.innerHTML = days [day] + ', ' + date + ' ' + months[month];
+}, 1000);
+
+
+getResponse () 
+async function getResponse () {
+    navigator.geolocation.getCurrentPosition ((success) => {
+        let {latitude, longitude} = success.coords;
+        
+
+       fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`)
+       .then (res => res.json())
+       .then (data => {
+            console.log(data);
+            city.innerHTML = data.timezone;
+        });
+       
+    });
+}
+
+
