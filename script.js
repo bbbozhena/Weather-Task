8b45463ed3ef27e9e86862955e188f7f343ef423
bbbozhena <bbbozhenka@gmail.com>
2022-07-05 17:59:04 +0300

    add default pos

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+'use strict';
+
+
 const city = document.getElementById ('city');
 const dateEl = document.getElementById ('date');
 const tempEl = document.getElementById ('temp');
@@ -14,52 +17,73 @@ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 
-setInterval (() => {
+
     const time = new Date ();
     const month = time.getMonth();
     const date =  time.getDate();
     const day = time.getDay();
 
     dateEl.innerHTML = days [day] + ', ' + date + ' ' + months[month];
-}, 1000);
+
+ 
+   
 
 
-getResponse () 
-async function getResponse () {
+ 
+getGeo ();
+
+function getGeo () {
     navigator.geolocation.getCurrentPosition ((success) => {
         let {latitude, longitude} = success.coords;
-        
+   
+        getWeather (latitude, longitude);
+       
+         });
+            
+        }
 
-       fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`)
+
+       
+let options = {
+    timeout: 1000
+};
+
+function success(pos) {
+    let crd = pos.coords;
+
+    getWeather (`${crd.latitude} ${crd.longitude} `);
+    
+}
+ function error() {
+     getWeather(51.509865, -0.118092 );
+ }
+
+ navigator.geolocation.getCurrentPosition(success, error, options);
+    
+    
+
+
+function getWeather (latitude, longitude) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`)
        .then (res => res.json())
        .then (data => {
             console.log(data);
+            if (city) {
             city.innerHTML = data.timezone;
+        }
+            if (tempEl) {
             tempEl.innerHTML = Math.floor (data.current.temp) + '°';
-            weatherEl.innerHTML = data.current ['weather'][0]['main'];
-            windNum.innerHTML = Math.floor (data.current ['wind_speed']) + ' km/h';
-            humNum.innerHTML =  data.current['humidity'] + ' %';
+        }
+           if (weatherEl) {
+            weatherEl.innerHTML = data.current ['weather'][0]['main']; 
+        } 
+            if (windNum) {
+                windNum.innerHTML = Math.floor (data.current ['wind_speed']) + 'km/h';
+        } 
+            if (humNum) {
+                humNum.innerHTML =  data.current['humidity'] + ' %';
+        } 
 
-        });
-       
-    });
+             });
 }
-
-
-
-
-// function showWeatherItems (data) {
-//     let {
-//         temp
-//     } = data.current;
-
-
-// weatherItem.innerHTML = 
-// `
-// <div class="weather-item">
-//         <div>temp</div>
-//         <div>${temp}%</div>
-//     </div>
-// `;
-// } 
 
